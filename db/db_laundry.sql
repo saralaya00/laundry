3b28440e3c110b2e8401b2a71d5d732222eeb610
Saurabh K Saralaya <s97work@gmail.com>
2018-06-09 17:15:20 +0530

    db and schema

--- db/db_laundry.sql
+++ db/db_laundry.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 09, 2018 at 08:45 AM
+-- Generation Time: Jun 09, 2018 at 01:44 PM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 5.6.35
 
@@ -53,26 +53,53 @@ CREATE TABLE `employee` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `items`
+-- Table structure for table `feedback`
 --
 
-CREATE TABLE `items` (
-  `item_id` int(10) NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `quantity` int(3) NOT NULL
+CREATE TABLE `feedback` (
+  `order_id` int(10) NOT NULL,
+  `description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `payments`
+-- Table structure for table `items`
 --
 
-CREATE TABLE `payments` (
-  `payment_id` int(10) NOT NULL,
-  `mode` varchar(30) NOT NULL,
-  `date` date NOT NULL,
-  `amount` int(5) NOT NULL
+CREATE TABLE `items` (
+  `item_id` int(10) NOT NULL,
+  `service_id` int(10) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `price` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(10) NOT NULL,
+  `customer_id` int(10) NOT NULL,
+  `item_id` int(10) NOT NULL,
+  `order_date` date NOT NULL,
+  `delivery_date` date NOT NULL,
+  `status` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_tracking`
+--
+
+CREATE TABLE `order_tracking` (
+  `tracking_id` int(10) NOT NULL COMMENT 'Record Insertions Only',
+  `order_id` int(10) NOT NULL,
+  `employee_id` int(10) NOT NULL,
+  `status` varchar(30) NOT NULL COMMENT 'Tracking Status and not item::service status'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -83,10 +110,8 @@ CREATE TABLE `payments` (
 
 CREATE TABLE `services` (
   `service_id` int(10) NOT NULL,
-  `service_type` varchar(20) NOT NULL,
-  `order_date` date NOT NULL,
-  `delivery_date` date NOT NULL,
-  `status` varchar(20) NOT NULL
+  `name` int(30) NOT NULL,
+  `description` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -106,22 +131,109 @@ ALTER TABLE `employee`
   ADD PRIMARY KEY (`emoloyee_id`);
 
 --
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD KEY `FK_feedback_order_id` (`order_id`);
+
+--
 -- Indexes for table `items`
 --
 ALTER TABLE `items`
-  ADD PRIMARY KEY (`item_id`);
+  ADD PRIMARY KEY (`item_id`),
+  ADD KEY `FK_service_id` (`service_id`);
 
 --
--- Indexes for table `payments`
+-- Indexes for table `orders`
 --
-ALTER TABLE `payments`
-  ADD PRIMARY KEY (`payment_id`);
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `FK_customer_id` (`customer_id`),
+  ADD KEY `FK_item_id` (`item_id`) USING BTREE COMMENT 'PK_items([service_id::item_id]) needs to be resolved or it will create problems for foreign key';
+
+--
+-- Indexes for table `order_tracking`
+--
+ALTER TABLE `order_tracking`
+  ADD PRIMARY KEY (`tracking_id`),
+  ADD KEY `FK_order_id` (`order_id`),
+  ADD KEY `FK_employee_id` (`employee_id`);
 
 --
 -- Indexes for table `services`
 --
 ALTER TABLE `services`
   ADD PRIMARY KEY (`service_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `customer_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `emoloyee_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `item_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order_tracking`
+--
+ALTER TABLE `order_tracking`
+  MODIFY `tracking_id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Record Insertions Only';
+
+--
+-- AUTO_INCREMENT for table `services`
+--
+ALTER TABLE `services`
+  MODIFY `service_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD CONSTRAINT `FK_feedback_order_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
+
+--
+-- Constraints for table `items`
+--
+ALTER TABLE `items`
+  ADD CONSTRAINT `FK_service_id` FOREIGN KEY (`service_id`) REFERENCES `services` (`service_id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `FK_customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`),
+  ADD CONSTRAINT `FK_item_id` FOREIGN KEY (`item_id`) REFERENCES `items` (`item_id`);
+
+--
+-- Constraints for table `order_tracking`
+--
+ALTER TABLE `order_tracking`
+  ADD CONSTRAINT `FK_employee_id` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`emoloyee_id`),
+  ADD CONSTRAINT `FK_order_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
