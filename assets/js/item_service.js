20c912537b4aea6b21421c35efbe6f538142aa75
sraoraksha <36411681+sraoraksha@users.noreply.github.com>
2018-07-06 17:00:51 +0530

    change employee

--- assets/js/item_service.js
+++ assets/js/item_service.js
@@ -12,8 +12,23 @@ $(document).ready(function(){
         "columnDefs":[{  
                 "targets":[0, 1, 2, 3, 4],  
                 "orderable":false,  
-                "className": "text-center",
             },  
         ],  
     });  
+
+  
+    $(document).on('click', '.edit', function(){ 
+        mdl_clear();
+        mdl_title.html('Edit Order');
+        mdl_submit.html('Update');
+    
+        // let id = $(this).data("id");
+        $.post(baseURL + 'Item_service_controller/md_edit', function (data){
+            mdl_body.html(data);
+           // getEmployee(order_id);
+        });   
+    
+    });
+    
+    
 });
