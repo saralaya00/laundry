70cfcd325732fac5e2ad898073183cd74d7aadf9
sraoraksha <36411681+sraoraksha@users.noreply.github.com>
2018-07-13 12:15:44 +0530

    ItemService_Controller

--- application/views/common/end_wrapper.php
+++ application/views/common/end_wrapper.php
@@ -8,7 +8,7 @@
     <!-- Custom JS -->
     <script src="assets/js/common.js"></script>
     <script src="assets/js/order.js"></script>
-    <script src="assets/js/item_service.js"></script>
+    <!-- <script src="assets/js/item_service.js"></script> -->
     <!--<script src="assets/js/bootbox.min.js"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></script>
     
