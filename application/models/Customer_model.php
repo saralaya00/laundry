ca51d02b09b22129b35498e771640d2fd91b9f28
pooja-tendulkar <36763885+pooja-tendulkar@users.noreply.github.com>
2018-07-16 15:02:16 +0530

    display_sservices

--- application/models/Customer_model.php
+++ application/models/Customer_model.php
@@ -1,47 +1,122 @@
 <?php
 class Customer_model extends CI_Model
 {
-    var $table = "users";
+    /* var $table = "users";
     var $select_column=array("id","first_name","last_name","image");
-    var $order_column=array(null,"first_name","last_name",null,null);
-  
-    function make_query(){
-        $this->db->select($this->select_column);
-        $this->db->from($this->table);
-        if(isset($_POST["search"]["value"]))
+     var $order_column=array(null,"first_name","last_name",null,null);*/
+    
+
+    public function getItemServiceDetails()
+    {
+
+        $i_id_result = $this->getItemId();
+        $item_id = array();
+        foreach ($i_id_result->result_array() as $row)
         {
-            $this->db->like("first_name",$_POST["search"]["value"]);
-            $this->db->or_like("last_name",$_POST["search"]["value"]);
+            $item_id[] = $row['item_id'];
         }
-        if(isset($_POST["order"]))
-        {  
-          $this->db->order_by($this->order_column[$_POST['order']['0']['column'
-                 ]], $_POST['order']['0']['dir']);
-        }
-        else
+
+        $s_id_result = $this->getServiceId();
+        $service_id = array();
+        foreach ($s_id_result->result_array() as $row)
         {
-          $this->db->order_by("id","DESC");
+            $service_id[] = $row['service_id'];
         }
+        
+
+        $this->db->select('is.id,it.item_name,s.service_name,is.price');
+        $this->db->join('items as it','is.item_id=it.item_id');
+        $this->db->join('services as s','s.service_id=is.service_id');
+        $this->db->where_in('is.item_id',$item_id)->where_in('is.service_id',$service_id);
+        $this->db->from('item_service as is');
+        $query = $this->db->get();
+        return $query->result_array();
     }
 
-    function make_datatables()
+    function getItemId()
     {
-       $this->make_query();
-       if($_POST["length"] != -1)  
-       {  
-            $this->db->limit($_POST['length'], $_POST['start']);  
-       }  
-       $query = $this->db->get();  
-       return $query->result();  
+        $this->db->select('item_id');
+        $this->db->from('item_service');
+        $query = $this->db->get();
+        return $query;
     }
-    function get_filtered_data(){
-        $this->make_query();
-        $query=$this->db->get();
-        return $query->num_rows();
+
+    function getServiceId()
+    {
+        $this->db->select('service_id');
+        $this->db->from('item_service');
+        $query = $this->db->get();
+        return $query;
     }
-    function get_all_data(){
-        $this->db->select("*");
-        $this->db->from($this->table);
-        return $this->db->count_all_results();
+
+
+    function getServices()
+    {
+        $this->db->select('service_name');
+        $this->db->from('services');
+        $query = $this->db->get();
+        return $query;
     }
+
+    // function getItem()
+    // {
+    //     $query=$this->db->get('items');  
+    //     return $query->result();
+    // }
+
+    // function getService()
+    // {
+    //     $query=$this->db->get('services');  
+    //     return $query->result();
+    // }
+
+    // public function updateItem_service($input)
+    // {
+    //     $id = $input['id'];
+
+    //     $item_name = $input['item_name'];
+    //     $item_id = $this->getUpdateItemId($item_name);
+
+    //     $service_name = $input['service_name'];
+    //     $service_id = $this->getUpdateServiceId($service_name);
+
+    //     $price = $input['price'];
+    //     $data = array('item_id' => $item_id,
+    //                 'service_id' => $service_id);
+
+    //     $single_data = call_user_func_array('array_merge',call_user_func_array('array_merge', $data));
+       
+    //     $this->db->where('id',$id);  
+    //     $query = $this->db->update("item_service",$single_data);
+        
+    //     $this->db->set('price',$price);
+    //     $this->db->where('id',$id);  
+    //     $query2 = $this->db->update("item_service");
+        
+    // }
+
+    // public function getUpdateItemId($item_name)
+    // {
+    //     $this->db->select('item_id');
+    //     $this->db->from('items');
+    //     $this->db->where('item_name',$item_name,NULL, FALSE);
+    //     return $this->db->get()->result_array();
+    // }
+
+    // public function getUpdateServiceId($service_name)
+    // {
+       
+    //     $this->db->select('service_id');
+    //     $this->db->from('services');
+    //     $this->db->where('service_name',$service_name);
+    //     return $this->db->get()->result_array();
+    // }
+
+    // public function deleteItem_service($id)
+    // {
+    //     $this->db->where('id', $id);
+    //     $this->db->delete('item_service'); 
+    // }
+
 }
+?>
