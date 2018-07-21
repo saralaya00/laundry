392eb4539b7ade3c73aeb2db5587c8d9a9414b27
Saurabh K Saralaya <s97work@gmail.com>
2018-07-21 16:53:08 +0530

    service_raksha fix

--- application/models/Service_model.php
+++ application/models/Service_model.php
@@ -9,21 +9,21 @@
 
         function add_service($service_data)
         {
-            $this->db->select("*");
-            $this->db->from('services');
-            $this->db->where('service_name',$service_data['service_name']);
-            $result = $this->db->get()->result_array();
+            // $this->db->select("*");
+            // $this->db->from('services');
+            // $this->db->where('service_name',$service_data['service_name']);
+            // $result = $this->db->get()->result_array();
 
-            if($result > 0){
-                $this->db->set('service_name',$service_data['service_name']);
-                $this->db->set('flag',1);
-                $this->db->where('service_name',$service_data['service_name']);
-                $query2 = $this->db->update("Services");   
-            }
-            else{
+            // if($result > 0){
+            //     $this->db->set('service_name',$service_data['service_name']);
+            //     $this->db->set('flag',1);
+            //     $this->db->where('service_name',$service_data['service_name']);
+            //     $query2 = $this->db->update("services");   
+            // }
+            // else{
                 $this->db->insert('services',$service_data);
                 return $this->db->affected_rows();
-            }
+            // }
            
         }
 
